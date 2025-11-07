be8ef1b8caee49f10764aadae464afec2dceef91
Raul Bolivar Navas <raul.bolivar@pragma.com.co>
2025-11-07 11:39:13 -0500

    integracion 1

--- api-service/src/main/java/com/example/api/config/RedisConfig.java
+++ api-service/src/main/java/com/example/api/config/RedisConfig.java
@@ -1,22 +1,40 @@
 package com.example.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public ReactiveRedisConnectionFactory redisConnectionFactory() { return new LettuceConnectionFactory(); }
+
+    @Value("${spring.data.redis.host}") private String redisHost;
+    @Value("${spring.data.redis.port}") private String redisPort;
 
     @Bean
-    public ReactiveStringRedisTemplate reactiveStringRedisTemplate(ReactiveRedisConnectionFactory cf) {
-        var template = new ReactiveStringRedisTemplate(cf);
-        template.setKeySerializer(StringRedisSerializer.UTF_8);
-        template.setValueSerializer(StringRedisSerializer.UTF_8);
-        return template;
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, Integer.parseInt(redisPort));
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        var keySerializer = new StringRedisSerializer();
+        var valueSerializer = new StringRedisSerializer();
+
+        RedisSerializationContext<String, String> context = RedisSerializationContext
+                .<String, String>newSerializationContext(keySerializer)
+                .key(keySerializer)
+                .value(valueSerializer)
+                .hashKey(keySerializer)
+                .hashValue(valueSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 }
--- worker-service/src/main/java/com/example/worker/config/RedisConfig.java
+++ worker-service/src/main/java/com/example/worker/config/RedisConfig.java
@@ -1,19 +1,41 @@
 package com.example.worker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean public ReactiveRedisConnectionFactory redisConnectionFactory() { return new LettuceConnectionFactory(); }
-    @Bean public ReactiveStringRedisTemplate reactiveStringRedisTemplate(ReactiveRedisConnectionFactory cf) {
-        var t = new ReactiveStringRedisTemplate(cf);
-        t.setKeySerializer(StringRedisSerializer.UTF_8);
-        t.setValueSerializer(StringRedisSerializer.UTF_8);
-        return t;
+
+    @Value("${spring.data.redis.host}") private String redisHost;
+    @Value("${spring.data.redis.port}") private String redisPort;
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, Integer.parseInt(redisPort));
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        var keySerializer = new StringRedisSerializer();
+        var valueSerializer = new StringRedisSerializer();
+
+        RedisSerializationContext<String, String> context = RedisSerializationContext
+                .<String, String>newSerializationContext(keySerializer)
+                .key(keySerializer)
+                .value(valueSerializer)
+                .hashKey(keySerializer)
+                .hashValue(valueSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 }
